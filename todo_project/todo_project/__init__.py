8a168419b6f34053385b4c6454ca87e89fade1a8
Anderson de Araújo Alves <anderson.araujoa@gmail.com>
2025-04-08 12:09:38 -0400

    feat:Adjust __init__ app config

--- todo_project/todo_project/__init__.py
+++ todo_project/todo_project/__init__.py
@@ -7,10 +7,14 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '45cf93c4d41348cd9980674ade9a7356'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# Utilizar 'Strict' dependendo do scopo do projeto
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+# Garante enviar cookies em conexões HTTPS
+app.config['SESSION_COOKIE_SECURE'] = True
 db = SQLAlchemy(app)
 
 login_manager = LoginManager(app)
-login_manager.login_view = 'login' 
+login_manager.login_view = 'login'
 login_manager.login_message_category = 'danger'
 
 bcrypt = Bcrypt(app)
